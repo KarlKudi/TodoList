d0c93be7496fc297c129eb794f6459f5d378f3da
Carl Egonu <egonucarl@gmail.com>
2023-10-30 07:40:55 +0000

    found issue with spaces in task text FIXED :)

--- public/scripts/taskComplete.js
+++ public/scripts/taskComplete.js
@@ -1,14 +1,30 @@
 let listOfTasks = document.querySelectorAll(".task");
 
-listOfTasks.forEach(element => {
-    //Adds event listener to label, when pressed it toggles the taskCompleted clas on the label. It also toggles the checked state of the relevant checkbox to true/false
-    element.addEventListener("click", () =>{
-        element.classList.toggle("taskCompleted");
-        document.querySelector("#" + element.innerText + ' input[type="checkbox"]').checked = !document.querySelector("#" + element.innerText + ' input[type="checkbox"').checked;
+console.log(listOfTasks);
+
+for (let i = 0; i < listOfTasks.length; i++) {
+    const element = listOfTasks[i];
+
+    element.querySelector("label").addEventListener("click", () =>{
+        element.querySelector("label").classList.toggle("taskCompleted");
+        element.querySelector('input[type="checkbox"]').checked = !element.querySelector('input[type="checkbox"').checked;
     });
 
     //Adds event listener to the checkbox, when it's pressed toggles the taskCompleted class on the label
-    document.querySelector("#" + element.innerText + ' input[type="checkbox"]').addEventListener("click", () =>{
-        element.classList.toggle("taskCompleted");
+    element.querySelector('input[type="checkbox"]').addEventListener("click", () =>{
+        element.querySelector("label").classList.toggle("taskCompleted");
     });
-});
+}
+
+// listOfTasks.forEach(element => {
+//     //Adds event listener to label, when pressed it toggles the taskCompleted class on the label. It also toggles the checked state of the relevant checkbox to true/false
+//     element.addEventListener("click", () =>{
+//         element.classList.toggle("taskCompleted");
+//         document.querySelector("#" + element.innerText + ' input[type="checkbox"]').checked = !document.querySelector("#" + element.innerText + ' input[type="checkbox"').checked;
+//     });
+
+//     //Adds event listener to the checkbox, when it's pressed toggles the taskCompleted class on the label
+//     document.querySelector("#" + element.innerText + ' input[type="checkbox"]').addEventListener("click", () =>{
+//         element.classList.toggle("taskCompleted");
+//     });
+// });
